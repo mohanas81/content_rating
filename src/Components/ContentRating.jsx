b2b781e9be49d5d97d75058d09b6fadc9f773fa3
mohanas81 <mohamad.anas.as@gmail.com>
2026-01-08 16:06:34 +0700

    initialize

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,29 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      handleLike: () => {
+        this.setState({ likes: this.state.likes + 1 });
+      },
+      handleDislike: () => {
+        this.setState({ dislikes: this.state.dislikes + 1 });
+      }
+    }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className="content-rating">
+        <p>
+          Component
+        </p>
+        <div className='rating-buttons'>
+          <button className='like-button' onClick={this.state.handleLike}>Like ({this.state.likes})</button>
+          <button className='dislike-button' onClick={this.state.handleDislike}>Dislike ({this.state.dislikes})</button>  
+          </div>
+      </div>
      </>
     );
   }
